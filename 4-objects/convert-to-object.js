d65b815050395bf9f9263dfd43116a4da8c7baab
Orel Vladislav <orel.vladyslav.fe@gmail.com>
2024-07-27 10:14:13 +0300

    add convert to object task

---
+++ 4-objects/convert-to-object.js
@@ -0,0 +1,13 @@
+// convert to object
+
+const players = [
+  ["player1", 80],
+  ["player2", 90],
+  ["player3", 85],
+];
+
+// start code
+
+// end code
+
+// { player1: 80, player2: 90, player3: 85 }
