66f1cf7ac4676f8e86b2c2a278742f64da86459a
Orel Vladislav <orel.vladyslav.fe@gmail.com>
2024-07-27 14:41:39 +0300

    add filter array of objects task

---
+++ 2-arrays/filter-array-of-objects.js
@@ -0,0 +1,15 @@
+// new array users that greater than 18
+// use .filter method
+
+const users = [
+  { name: "John", age: 20 },
+  { name: "Jane", age: 30 },
+  { name: "Jim", age: 15 },
+  { name: "Jill", age: 25 },
+];
+
+// start code
+
+// end code
+
+// [{ name: "John", age: 20 }, { name: "Jane", age: 30 }, { name: "Jill", age: 25 }]
